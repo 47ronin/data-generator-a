1e4909e1d454a3bb809e34e4683912928f0e3c8d
Glenn Batuyong <glenn@47ronin.com>
2017-04-12 10:48:49 -0700

    New features: FontAwesome and WeatherIcons output data

--- app/process.php
+++ app/process.php
@@ -10,16 +10,22 @@
       @$wTemp    = $payload->wTemp;
       @$wSummary = $payload->wSummary;
       @$wAlert   = $payload->wAlert;
+      @$wIcon    = $payload->wIcon;
       @$sQuote   = $payload->stockQuote;
+      @$sIcon    = $payload->stockChangeI;
       @$sChange  = $payload->stockChange;
+      @$sChangeE = $payload->stockChangeE;
       @$sChangeP = $payload->stockChangeP;
 
       $basepath            = 'output';
       $output_weather      = trim($basepath.'/'.'output_weather.txt');
       $output_wSummary     = trim($basepath.'/'.'output_wSummary.txt');
       $output_wAlert       = trim($basepath.'/'.'output_wAlert.txt');
+      $output_wIcon        = trim($basepath.'/'.'output_wIcon.txt');
       $output_stockQuote   = trim($basepath.'/'.'output_stockQuote.txt');
+      $output_stockChangeI = trim($basepath.'/'.'output_stockChangeI.txt');
       $output_stockChange  = trim($basepath.'/'.'output_stockChange.txt');
+      $output_stockChangeE = trim($basepath.'/'.'output_stockChangeE.txt');
       $output_stockChangeP = trim($basepath.'/'.'output_stockChangeP.txt');
 
       function check_and_write($final_path, $data, $flags = 0)
@@ -47,11 +53,20 @@
       if (isset($wAlert)) {
           check_and_write($output_wAlert, $wAlert);
       }
+      if (isset($wIcon)) {
+          check_and_write($output_wIcon, $wIcon);
+      }
       if (isset($sQuote)) {
           check_and_write($output_stockQuote, $sQuote);
       }
+      if (isset($sIcon)) {
+          check_and_write($output_stockChangeI, $sIcon);
+      }
       if (isset($sChange)) {
           check_and_write($output_stockChange, $sChange);
+      }
+      if (isset($sChangeE)) {
+          check_and_write($output_stockChangeE, $sChangeE);
       }
       if (isset($sChangeP)) {
           check_and_write($output_stockChangeP, $sChangeP);
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,7 +9,7 @@
  */
 angular.module('dataGenApp')
   .controller('MainCtrl',
-    ['$scope', '$http', '$q', function ($scope, $http, $q) {
+    ['$scope', '$http', '$filter', '$q', function ($scope, $http, $filter, $q) {
 
     // Initialize everything
     var wAPIkey    = 'FORECAST_API_KEY', // forecast.io API key
@@ -17,13 +17,27 @@ angular.module('dataGenApp')
         wOptions   = '&exclude=minutely,hourly,daily,flags', // API exclusions
 		    stationURL = 'https://api.forecast.io/forecast/' + wAPIkey +
                       '/' + targetGeo + '?callback=JSON_CALLBACK' + wOptions,
-        weather    = $http.jsonp(stationURL);
+        weather    = $http.jsonp(stationURL),
+        iconArray  = [
+			{oIcon: 'clear-day',           wIcon: ''},
+			{oIcon: 'clear-night',         wIcon: ''},
+			{oIcon: 'rain',                wIcon: ''},
+			{oIcon: 'snow',                wIcon: ''},
+			{oIcon: 'sleet',               wIcon: ''},
+			{oIcon: 'wind',                wIcon: ''},
+			{oIcon: 'fog',                 wIcon: ''},
+			{oIcon: 'cloudy',              wIcon: ''},
+			{oIcon: 'partly-cloudy-day',   wIcon: ''},
+			{oIcon: 'partly-cloudy-night', wIcon: ''}
+		];
 
     // Retrieve weather data
     $q.all([weather])
     .then(function (promise) {
       var wRaw = promise[0].data.currently;
+      var searchIcon = $filter('filter')(iconArray, {oIcon: wRaw.icon});
       $scope.wTemp = Math.round(wRaw.temperature);
+      $scope.wIcon = searchIcon[0].wIcon;
       $scope.wSummary = wRaw.summary;
       if (promise[0].data.alerts) {
         $scope.wAlert = promise[0].data.alerts[0].description.replace(/\\n/g,' ');
@@ -35,7 +49,8 @@ angular.module('dataGenApp')
       var payLoad = {
         wTemp    : $scope.wTemp,
         wSummary : $scope.wSummary,
-        wAlert   : $scope.wAlert
+        wAlert   : $scope.wAlert,
+        wIcon    : $scope.wIcon
       };
       $http({
         method: 'POST',
@@ -70,8 +85,11 @@ angular.module('dataGenApp')
       var sRaw = promise[0].data;
       $scope.stockQuote = parseFloat(Math.round(sRaw.LastPrice * 100) / 100).toFixed(2);
       if (sRaw.Change >= 0) {
-        $scope.stockChange = '+' + parseFloat(Math.round(sRaw.Change * 100) / 100).toFixed(2);
+        $scope.stockChangeIcon = '';
+        $scope.stockChange = parseFloat(Math.round(sRaw.Change * 100) / 100).toFixed(2);
+        $scope.stockChangeE = '+' + $scope.stockChange;
       } else {
+        $scope.stockChangeIcon = '';
         $scope.stockChange = parseFloat(Math.round(sRaw.Change * 100) / 100).toFixed(2);
       }
       if (sRaw.ChangePercent >=0) {
@@ -83,7 +101,9 @@ angular.module('dataGenApp')
       // Push data to PHP processor
       var payLoad = {
         stockQuote   : $scope.stockQuote,
+        stockChangeI : $scope.stockChangeIcon,
         stockChange  : $scope.stockChange,
+        stockChangeE : $scope.stockChangeE,
         stockChangeP : $scope.changePercent
       };
       $http({
